2cf2779f56af12b671836783e656ab35f755d689
Matias Robles <matias.robles1601@gmail.com>
2020-01-18 21:10:41 -0300

    PORFIN!, todo funcionando con impresiones raras, hay que corregir eso

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TareaIA
+Esta es una tarea en la que te insertan un conjunto de numeros y el programa deja de leer cuando te insertan el 0
+Lo que hace el programa es devolverte un subconjunto de numeros que sumado de 0, de no existir queda en bucle y 
+eventualmente explota
--- TareaIA.py
+++ TareaIA.py
@@ -9,8 +9,10 @@ import sys
 # Declaración de variables globales
 numeros_in = [] # Lista que almacena el conjunto ingresado
 recorridos = [] # Lista que almacena los números recorridos
+indices = []
 
 cont = 0
+contador = 0
 sum_acum = 0
 heur = 0
 head = 0
@@ -23,8 +25,12 @@ pnt = '..........'
 def init():
 	heur = 0
 	head = 0
+	contador = 0
 	numeros_in = llamada()
-	resolve(numeros_in,heur,head)
+	for i in numeros_in:
+		indices.append('x')
+	print(indices)
+	resolve(numeros_in,heur,head,indices)
 
 # Método que lee el conjunto de entrada
 def llamada():
@@ -52,18 +58,20 @@ def llamada():
 	return numeros_in
 
 
-def resolve(numeros_in,heur,head):
+def resolve(numeros_in,heur,head,indices):
 	head = random.randrange(len(numeros_in))
 	print("head afsdjasdf %d"%head)
-	heur = 99999999999999999
+	heur = sys.maxsize
 	sum_acum = numeros_in[head]
 	recorridos.append(numeros_in[head])
+	indices[head] = head
 	print('Se agrego el %d como primer valor'%numeros_in[head])
+	print(indices)
 	print('head: %d,heur: %d,sum_acum: %d'%(head,heur,sum_acum))
 	head,heur=heuristic(heur,head,sum_acum)
 	print('head: %d,heur: %d,sum_acum: %d'%(head,heur,sum_acum))
-	if heur != 0:
-		next_num(head,heur,sum_acum)
+	while heur != 0:
+		heur,indices=next_num(head,heur,sum_acum)
 	print('RESULTADO')
 	print(numeros_in)
 	print(recorridos)
@@ -74,8 +82,11 @@ def heuristic(heur,head,sum_acum):
 	num = numeros_in[0]
 	print(numeros_in)
 	for num in numeros_in:
+		print(indices)
 		print("esto es un numero o es un indice? %d"%num)
-		if (num in recorridos):
+		ind = numeros_in.index(num)
+		print("indice %d"%ind)
+		if ( ind in indices):
 			print('este no %d'%num)
 		else:
 			heur_aux = sum_acum + num
@@ -86,6 +97,7 @@ def heuristic(heur,head,sum_acum):
 				heur = abs(heur_aux)
 				head = numeros_in.index(num)
 				recorridos.append(numeros_in[head])
+				indices[head] = head
 				print('RESULTADO')
 				print(numeros_in)
 				print(recorridos)
@@ -96,23 +108,25 @@ def heuristic(heur,head,sum_acum):
 				head = numeros_in.index(num)
 	print('se agrego %d'%(numeros_in[head]))
 	recorridos.append(numeros_in[head])
+	indices[head] = head
 	return head,heur
 
 def next_num(head,heur,sum_acum):
 	print('next')
 	sum_acum = sum_acum + numeros_in[head]
-	print("final s%d"%sum_acum)
-	if(sum_acum == 0):
-		print('RESULTADO')
-		print(numeros_in)
-		print(recorridos)
-		return 'end'
+	print("final %d"%sum_acum)
 	head,heur=heuristic(heur,head,sum_acum)
 	if (heur != 0 and (len(recorridos) == len(numeros_in))):
+		print("ASJDIOASJDIOASJDOIASJDOIASJDOIJASDOIJASDOIJASDOIJASOIDJASOIDJASODJOAIS")
 		recorridos.clear()
+		c = 0
+		for i in numeros_in:
+			indices[c] = 'x'
+			c += 1
 		heur = 0
 		sum_acum = 0
-	resolve(numeros_in,heur,head)
+		resolve(numeros_in,heur,head,indices)
+	return heur,indices
 
 
 init()
